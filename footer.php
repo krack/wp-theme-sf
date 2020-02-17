ecceca78bfe123b4228b6a85a498597487b492f1
Sylvain Gandon <krack_6@hotmail.com>
2020-02-17 23:25:44 +0100

    mention legal + fix csss

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div> <!-- /.container -->
 <footer class="blog-footer">
     <hr />
-    <span class="copyright">Copyright © <?php echo date("Y"); ?> by <a href="https://www.services-peps.fr/" target="_blank">PEPS</a><span>
+    <span class="copyright">Copyright © <?php echo date("Y"); ?> Stud Book Selle Français | Tous droits réservés | <a href="/mention-legales">Mentions légales</a> | Réalisé par <a href="https://www.services-peps.fr/" target="_blank">PEPS</a><span>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
