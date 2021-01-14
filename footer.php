fbbdd5cbfdf4e1f391bf9024723a9212335564d6
Sylvain Gandon <krack_6@hotmail.com>
2021-01-14 10:54:44 +0100

    save all change

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
-
-
 </div> <!-- /.container -->
 <?php 
 
@@ -9,7 +7,7 @@ if(function_exists("shf_login_block")){
 
 ?>
 <footer class="blog-footer">
-    <span class="copyright">Copyright © <?php echo date("Y"); ?> Stud Book Selle Français | Tous droits réservés | <a href="/mention-legales">Mentions légales</a> | © photos : <a href="https://www.ouest-image.com/" target="_blank">Les Garennes</a> | Réalisé par <a href="https://www.services-peps.fr/" target="_blank">PEPS</a><span>
+    <span class="copyright">Copyright © <?php echo date("Y"); ?> Stud Book Selle Français | Tous droits réservés | <a href="/mention-legales">Mentions légales</a> | © photos : <a href="https://www.ouest-image.com/" target="_blank">Les Garennes</a> | Réalisé par <a href="https://www.services-peps.fr/" target="_blank">PEPS</a><span> | Site réalisé grâce au soutien du <a href="https://www.fondseperon.com/" target="_blank"><img alt="Fonds Éperon" src="/wp-content/themes/sellesfrancais/assets/images/Fonds Éperon logo.png" /></a> et de l'<a href="https://www.ifce.fr/" target="_blank"><img alt="IFCE" src="/wp-content/themes/sellesfrancais/assets/images/logo-ifce-mobile.png" /></a>.
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
